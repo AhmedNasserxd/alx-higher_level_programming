2550b1bcac3444c712f65eb5996b34abf311d194
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2024-01-02 21:02:56 +0200

    This is new python

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+def uppercase(s):
+"""Prints a string in uppercase followed by a new line."""
+for char in s:
+uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+print("{}".format(uppercase_char), end="")
+print()
+
+if __name__ == "__main__":
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
