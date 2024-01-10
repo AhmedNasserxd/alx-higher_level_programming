31614635269e332cc6f66f859f96ee8b794353a7
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2024-01-11 01:35:45 +0200

    excluding the unwanted ones

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-
 def complex_delete(a_dictionary, value):
     # Create a new dictionary without the specified value
     new_dict = {key: val for key, val in a_dictionary.items() if val != value}
 
-    return new_dict
+    # Create a sorted version of the new dictionary
+    sorted_new_dict = dict(sorted(new_dict.items()))
+
+    return sorted_new_dict
+
+
+# Import the print_sorted_dictionary function
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+
+# Updated print_sorted_dictionary function to exclude specific keys
+def print_sorted_dictionary(a_dictionary):
+    excluded_keys = {'lang', 'pref'}  # Add keys you want to exclude here
+    for key in sorted(a_dictionary):
+        if key not in excluded_keys:
+            print("{}: {}".format(key, a_dictionary[key]))
 
 
 if __name__ == "__main__":
