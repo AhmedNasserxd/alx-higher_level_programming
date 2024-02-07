8b6d091667a8d70e4265939337a4d45e1794b3f5
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2024-02-07 21:54:02 +0200

    adv

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Module 100-my_int"""
+
+class MyInt(int):
+    """Subclass of int with inverted == and != operators"""
+
+    def __eq__(self, other):
+        """Inverts the == operator"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Inverts the != operator"""
+        return super().__eq__(other)
