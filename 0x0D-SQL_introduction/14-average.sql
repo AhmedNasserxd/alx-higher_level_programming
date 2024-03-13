f9c65a4a179f71d1738cab19cba4a2cfdd5c8c7c
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-13 05:23:22 +0200

    Create 14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- computes the score average of all records
+-- database name will be passed as an argument
+-- result column name should be average
+
+SELECT AVG(score) AS 'average' FROM second_table;
