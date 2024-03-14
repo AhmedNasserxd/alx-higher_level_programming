aa48540ba6964d918506b62fc7018532db16f910
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-14 04:00:14 +0200

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this to import a SQL dump:
---      echo "CREATE DATABASE hbtn_0d_tvshows;"
+-- run this to import a SQL dump
 -- each record should display: tv_shows.title - tv_show_genres.genre_id
 -- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- can use only one SELECT statement
