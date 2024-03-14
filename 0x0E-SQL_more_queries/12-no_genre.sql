bbc3a20bbc84a1b879f222a42e5a4d5dd8ff3ae2
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-14 03:59:53 +0200

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,5 @@
 -- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this to import a SQL dump:
---      echo "CREATE DATABASE hbtn_0d_tvshows;"
+-- run this to import a SQL dump
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
 -- each record should display: tv_shows.title, and tv_show_genres.genre_id
 -- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
