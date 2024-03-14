8e41cefc855986a67d8761b2f6676612e55258c6
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-14 04:15:37 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,14 +7,13 @@
 -- can use max two SELECT statements
 -- the database name will be passed as an argument
 
-    SELECT tv_shows.title
-      FROM tv_shows
-     WHERE tv_shows.title NOT IN (
-    SELECT tv_shows.title
-      FROM tv_shows
-INNER JOIN tv_show_genres
-        ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
-	ON tv_show_genres.genre_id = tv_genres.id
-     WHERE tv_genres.name = "Comedy")
-  ORDER BY tv_shows.title ASC;
+SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
