16b7a8a163c37384a7a8fb2b749b26848a2c4282
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-14 03:56:58 +0200

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,5 @@
 -- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this to import a SQL dump:
---      echo "CREATE DATABASE hbtn_0d_tvshows;"
+-- run this to import a SQL dump
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows
 -- tv_genres table contains only one record where name = Comedy (but the id can be different)
 -- each record should display: tv_shows.title
