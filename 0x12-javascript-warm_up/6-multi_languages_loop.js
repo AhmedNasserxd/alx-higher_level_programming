c7c84da7cdfdea222717e2ea476c54400e5ccfe7
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2024-04-09 04:41:01 +0200

    JS

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const vary = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const vary = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const l of vary) {
   console.log(l);
 }
